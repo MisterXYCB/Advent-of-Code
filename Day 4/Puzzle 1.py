40856785f695153ac9411980ff30665580707c8d
MisterXYCB <ycb2134@gmail.com>
2023-12-04 16:18:51 +0100

    Day 4

---
+++ Day 4/Puzzle 1.py
@@ -0,0 +1,32 @@
+#Advent of Code 2023 Day 4 Puzzle 1
+import re
+
+cards = []
+score = 0
+result = 0
+
+with open("Day 4/data.txt") as f:
+    for line in f:
+        cards.append(re.sub("Card .+: ", "", line).replace("\n", "").split(" | "))
+
+
+for card in cards:
+    numbers = card[0].split(" ")
+    
+    winning_numbers = [int(ele) for ele in numbers if ele != "" and ele != " "]
+
+    numbers = card[1].split(" ")
+
+    your_numbers = [int(ele) for ele in numbers if ele != "" and ele != " "]
+
+    for number in your_numbers:
+        if number in winning_numbers and score != 0:
+            score = 2 * score
+        elif number in winning_numbers and score == 0:
+            score = 1
+
+    result += score
+
+    score = 0
+
+print(result) #21138
---
+++ Day 4/Puzzle 2.py
@@ -0,0 +1,43 @@
+#Advent of Code 2023 Day 4 Puzzle 2
+import re
+
+cards = []
+score = 0
+scores = []
+result = 0
+
+with open("Day 4/data.txt") as f:
+    for line in f:
+        cards.append(re.sub("Card .+: ", "", line).replace("\n", "").split(" | "))
+
+
+for card in cards:
+    numbers = card[0].split(" ")
+    
+    winning_numbers = [int(ele) for ele in numbers if ele != "" and ele != " "]
+
+    numbers = card[1].split(" ")
+
+    your_numbers = [int(ele) for ele in numbers if ele != "" and ele != " "]
+
+    for number in your_numbers:
+        if number in winning_numbers:
+            score += 1
+
+    scores.append([1, score])
+    score = 0
+
+for i, data in enumerate(scores):
+
+    count, score = data
+
+    for j in range(score):
+
+        if i + score - j >= len(scores):
+            continue
+
+        scores[i + score - j][0] = scores[i + score - j][0] + 1 * count
+
+    result += int(count)
+
+print(result) #7185540
