762fe50d6620630ed89591c69fadae09f825f216
MisterXYCB <ycb2134@gmail.com>
2024-12-12 19:56:14 +0100

    2024 Day 12

---
+++ Day 12/Puzzle 2.py
@@ -0,0 +1,117 @@
+from collections import defaultdict
+
+map = []
+areas = {}
+area2 = {}
+areasArea = defaultdict(int)
+differentAreas = 0
+
+with open("Day 12/data.txt") as f:
+    for y, line in enumerate(f):
+        line = line.replace("\n", "")
+        map.append(list(line))
+
+def getNeighbors(y, x, type):
+    neighbors = []
+    if(y > 0):
+        if(map[y-1][x] == type):
+            neighbors.append([y-1, x])
+    if(x > 0):
+        if(map[y][x-1] == type):
+            neighbors.append([y, x-1])
+    if(y < len(map) - 1):
+        if(map[y+1][x] == type):
+            neighbors.append([y+1, x])
+    if(x < len(line) - 1):
+        if(map[y][x+1] == type):
+            neighbors.append([y, x+1])
+    
+    return neighbors
+
+def getBorders(y, x, type):
+    borders = []
+    if(y > 0):
+        if(map[y-1][x] != type):
+            borders.append([y, x, 0])
+    else:
+        borders.append([y, x, 0])
+    if(x > 0):
+        if(map[y][x-1] != type):
+            borders.append([y, x, 1])
+    else:
+        borders.append([y, x, 1])
+    if(y < len(map) - 1):
+        if(map[y+1][x] != type):
+            borders.append([y+1, x, 0])
+    else:
+        borders.append([y+1, x, 0])
+    if(x < len(line) - 1):
+        if(map[y][x+1] != type):
+            borders.append([y, x+1, 1])
+    else:
+        borders.append([y, x+1, 1])
+    
+    
+    return borders
+
+def findUniqueAreas():
+    global differentAreas
+    knowPlaces = []
+    for y, line in enumerate(map):
+        for x, space in enumerate(line):
+            if([y,x] not in knowPlaces):
+                knowNeighbors = [[y,x]]
+                neighbors = getNeighbors(y, x, space)
+                knowNeighbors.extend(neighbors)
+                knowPlaces.extend(knowNeighbors)
+
+                areaName = "Area" + str(differentAreas)
+                areas[str(y)+str(x)] = areaName
+                areasArea[areaName] += 1
+
+                while len(neighbors) > 0:
+                    newNeighbors = []
+                    newNeighbors2 = []
+                    for neighbor in neighbors:
+                        areas[str(y)+str(x)] = areaName
+                        areasArea[areaName] += 1
+                        newNeighbors.extend(getNeighbors(neighbor[0], neighbor[1], space))
+                    for neighbor in newNeighbors:
+                        if(neighbor not in knowNeighbors):
+                            knowNeighbors.append(neighbor)
+                            newNeighbors2.append(neighbor)
+                    if(len(newNeighbors2) == 0):
+                        break
+
+                    knowPlaces.extend(newNeighbors2)
+                    knowNeighbors.extend(newNeighbors2)
+                    neighbors = newNeighbors2
+                
+                area2[areaName] = knowNeighbors
+                differentAreas += 1
+
+findUniqueAreas()
+
+sum = 0
+
+for key, value in area2.items():
+    borders = []
+    cnt = 0
+    for ele in value:
+        newBorders = getBorders(ele[0], ele[1], map[ele[0]][ele[1]])
+        for ele2 in newBorders:
+            if ele2 not in borders:
+                borders.append(ele2)
+
+    for border in borders:
+        if border[2] == 0:
+            if not (border[1] > 0 and [border[0], border[1] - 1, 0] in borders and [border[0] - 1, border[1], 1] not in borders):
+                cnt += 1
+        else:
+            if not (border[0] > 0 and [border[0] - 1, border[1], 1] in borders and [border[0], border[1], 0] not in borders):
+                cnt += 1
+    	
+    sum += cnt * areasArea[key]
+    
+    
+print(sum)
